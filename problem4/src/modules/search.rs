191379969acc79bac4f39087e94402470edd032e
Kesley Richardson <james@jkesley.com>
2019-02-27 11:42:10 -0600

    Fix to my A* search

--- problem4/src/modules/search.rs
+++ problem4/src/modules/search.rs
@@ -14,6 +14,7 @@ fn get_path_from_origin_map(goal: &'static str, start: &'static str,
         current = origin[current];
     }
     ret.push(current.clone());
+    ret.reverse();
     return ret;
 }
 
@@ -31,11 +32,13 @@ pub fn a_star(start: &'static str, end: &'static str,
     let mut queue: PriorityQueue<&'static str, i64> =  PriorityQueue::new();
     let mut cost: HashMap<&'static str, u32> = HashMap::new();
     let mut origin: HashMap<&'static str, &'static str> = HashMap::new();
+    let mut gscore_map: HashMap<&'static str, u32> = HashMap::new();
 
-    let mut visited = Vec::new();
+    let mut closed = Vec::new();
 
     queue.push(start, 0);
     cost.insert(start, 0);
+    gscore_map.insert(start, 0);
     origin.insert(start, "");
 
     while ! queue_empty(& queue){
@@ -44,7 +47,7 @@ pub fn a_star(start: &'static str, end: &'static str,
         let current_tuple = queue.pop().unwrap();
         let current: &'static str = current_tuple.0;
 
-        visited.push(current);
+        closed.push(current);
 
         if current == end {
             break;
@@ -53,14 +56,22 @@ pub fn a_star(start: &'static str, end: &'static str,
         for (ii, neighbor_name) in node_map[current].neighbors_names.clone().iter().enumerate() {
             let neighbor_dist = node_map[current].neighbors_dist[ii];
 
-            let this_cost = cost[current] + neighbor_dist +
-                            node_map[neighbor_name].heuristic(&node_map[end]);
-            // If we haven't visited this guy
-            // or if we found a better route to this guy
-            if ! visited.contains(neighbor_name) || this_cost < cost[neighbor_name]{
+            let gscore = cost[current] + neighbor_dist;
+            let hscore = node_map[neighbor_name].heuristic(&node_map[end]);
+            let this_cost =  gscore + hscore;
+
+            // If we haven't closed this guy, put him in the queue and update the mappings
+            if ! closed.contains(neighbor_name) {
+                gscore_map.insert(neighbor_name, gscore);
                 origin.insert(neighbor_name, current);
-                cost.insert(neighbor_name, this_cost);
+                cost.insert(neighbor_name, gscore);
                 queue.push(neighbor_name, -(this_cost as i64) );
+
+            // or if we found a better route to this guy just update the mappins
+            }else if gscore< gscore_map[neighbor_name] {
+                gscore_map.insert(neighbor_name, gscore);
+                origin.insert(neighbor_name, current);
+                cost.insert(neighbor_name, gscore);
             }
         }
     }
